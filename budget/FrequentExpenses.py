cf63461bd72310452ba74c8d5e0eccb1ab07fd6c
Jacek Biernacki <jacekbierny@gmail.com>
2022-11-17 07:17:57 +0100

    Count Expenses by Category

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import collections
+from . import Expense
+
+expanses = Expense.Expenses()
+expanses.read_expenses('data/spending_data.csv')
+spending_categories = []
+for expanse in expanses.list:
+    spending_categories.append(expanse.category)
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
+fig, ax = plt.subplots()
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
